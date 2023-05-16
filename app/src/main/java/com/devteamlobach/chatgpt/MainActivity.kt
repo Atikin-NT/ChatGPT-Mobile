4d3fdf0fd55aebe612cdf0f0e1a39281abfdb6ee
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2023-05-16 18:07:42 +0300

    mobile

---
+++ app/src/main/java/com/devteamlobach/chatgpt/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.devteamlobach.chatgpt
+
+import android.annotation.SuppressLint
+import android.net.http.SslError
+import android.os.Bundle
+import android.webkit.SslErrorHandler
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview_id)
+//        webView.webViewClient = WebViewClient()
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onReceivedError(
+                view: WebView?,
+                errorCode: Int,
+                description: String?,
+                failingUrl: String?
+            ) {
+                println("skip")
+            }
+            @SuppressLint("WebViewClientOnReceivedSslError")
+            override fun onReceivedSslError(
+                view: WebView,
+                handler: SslErrorHandler,
+                error: SslError
+            ) {
+                // DO NOT CALL SUPER METHOD
+                println("skip")
+                handler.proceed()
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
+                view.loadUrl(url!!)
+                return true
+            }
+        }
+
+        webView.settings.domStorageEnabled =true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+
+        webView.loadUrl("https://92.51.36.153/")
+
+
+        webView.settings.setSupportZoom(true)
+    }
+}
