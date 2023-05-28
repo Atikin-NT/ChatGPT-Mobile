cc52f9527af602210adf1f636c27077d699b5c04
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2023-05-28 19:57:02 +0300

    fix: url

--- app/src/main/java/com/devteamlobach/chatgpt/MainActivity.kt
+++ app/src/main/java/com/devteamlobach/chatgpt/MainActivity.kt
@@ -51,7 +51,7 @@ class MainActivity : AppCompatActivity() {
         webView.settings.javaScriptEnabled = true
         webView.settings.javaScriptCanOpenWindowsAutomatically = true
 
-        webView.loadUrl("https://92.51.36.153/chat/")
+        webView.loadUrl("https://92.51.36.153/chat")
 
 
         webView.settings.setSupportZoom(true)
